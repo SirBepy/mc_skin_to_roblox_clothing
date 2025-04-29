3cc3a3a241fedbc6b58cde3787a019c4e1a9e7c4
SirBepy <tecnomon99@gmail.com>
2025-04-29 12:50:28 +0200

    CORE: setup correct coordinates to unwrap your skin

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const config = {
         right: {
           label: 'R',
           drawRegion: { x: 165, y: 74, w: 64, h: 128 },
-          skinMap: { x: 44, y: 20, w: 4, h: 12 },
+          skinMap: { x: 16, y: 20, w: 4, h: 12 },
         },
         front: {
           label: 'FRONT',
@@ -28,7 +28,7 @@ const config = {
         left: {
           label: 'L',
           drawRegion: { x: 361, y: 74, w: 64, h: 128 },
-          skinMap: { x: 36, y: 52, w: 4, h: 12 },
+          skinMap: { x: 28, y: 20, w: 4, h: 12 },
           needsNewFormat: true,
           mirrorFrom: 'right',
         },
@@ -42,82 +42,157 @@ const config = {
           drawRegion: { x: 231, y: 204, w: 128, h: 64 },
           skinMap: { x: 28, y: 16, w: 8, h: 4 },
         },
+
+        right_arm_l: {
+          label: 'RL',
+          drawRegion: { x: 19, y: 355, w: 64, h: 128 },
+          skinMap: { x: 40, y: 20, w: 4, h: 12 },
+        },
+        right_arm_b: {
+          label: 'RB',
+          drawRegion: { x: 85, y: 355, w: 64, h: 128 },
+          skinMap: { x: 44, y: 20, w: 4, h: 12 },
+        },
+        right_arm_r: {
+          label: 'RR',
+          drawRegion: { x: 151, y: 355, w: 64, h: 128 },
+          skinMap: { x: 48, y: 20, w: 4, h: 12 },
+        },
+        right_arm_f: {
+          label: 'RF',
+          drawRegion: { x: 217, y: 355, w: 64, h: 128 },
+          skinMap: { x: 52, y: 20, w: 4, h: 12 },
+        },
+        right_arm_u: {
+          label: 'RU',
+          drawRegion: { x: 217, y: 289, w: 64, h: 64 },
+          skinMap: { x: 44, y: 16, w: 4, h: 4 },
+        },
+        right_arm_d: {
+          label: 'RD',
+          drawRegion: { x: 217, y: 485, w: 64, h: 64 },
+          skinMap: { x: 48, y: 16, w: 4, h: 4 },
+        },
+
+        left_arm_l: {
+          label: 'LL',
+          drawRegion: { x: 308, y: 355, w: 64, h: 128 },
+          skinMap: { x: 32, y: 52, w: 4, h: 12 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_arm_l',
+        },
+        left_arm_b: {
+          label: 'LB',
+          drawRegion: { x: 374, y: 355, w: 64, h: 128 },
+          skinMap: { x: 36, y: 52, w: 4, h: 12 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_arm_b',
+        },
+        left_arm_r: {
+          label: 'LR',
+          drawRegion: { x: 440, y: 355, w: 64, h: 128 },
+          skinMap: { x: 40, y: 52, w: 4, h: 12 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_arm_r',
+        },
+        left_arm_f: {
+          label: 'LF',
+          drawRegion: { x: 506, y: 355, w: 64, h: 128 },
+          skinMap: { x: 44, y: 52, w: 4, h: 12 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_arm_f',
+        },
+        left_arm_u: {
+          label: 'LU',
+          drawRegion: { x: 308, y: 289, w: 64, h: 64 },
+          skinMap: { x: 36, y: 48, w: 4, h: 4 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_arm_u',
+        },
+        left_arm_d: {
+          label: 'LD',
+          drawRegion: { x: 308, y: 485, w: 64, h: 64 },
+          skinMap: { x: 40, y: 48, w: 4, h: 4 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_arm_d',
+        },
       },
       extraLabels: [],
     },
     pants: {
       regions: {
-        rightUpperLeg: {
-          label: 'U',
-          drawRegion: { x: 214, y: 258, w: 65, h: 65 },
-          skinMap: { x: 4, y: 16, w: 4, h: 4 },
-        },
-        leftUpperLeg: {
-          label: 'U',
-          drawRegion: { x: 307, y: 258, w: 65, h: 65 },
-          skinMap: { x: 20, y: 48, w: 4, h: 4 },
-          needsNewFormat: true,
-          mirrorFrom: 'rightUpperLeg',
-        },
-        rightLegLeft: {
-          label: 'L',
-          drawRegion: { x: 14, y: 372, w: 65, h: 65 },
-          skinMap: { x: 8, y: 20, w: 4, h: 12 },
-        },
-        rightLegBack: {
-          label: 'B',
-          drawRegion: { x: 107, y: 372, w: 65, h: 65 },
-          skinMap: { x: 12, y: 20, w: 4, h: 12 },
-        },
-        rightLegRight: {
-          label: 'R',
-          drawRegion: { x: 200, y: 372, w: 65, h: 65 },
+        right_leg_l: {
+          label: 'RL',
+          drawRegion: { x: 19, y: 355, w: 64, h: 128 },
           skinMap: { x: 0, y: 20, w: 4, h: 12 },
         },
-        rightLegFront: {
-          label: 'F',
-          drawRegion: { x: 293, y: 372, w: 65, h: 65 },
+        right_leg_b: {
+          label: 'RB',
+          drawRegion: { x: 85, y: 355, w: 64, h: 128 },
           skinMap: { x: 4, y: 20, w: 4, h: 12 },
         },
-        leftLegFront: {
-          label: 'F',
-          drawRegion: { x: 386, y: 372, w: 65, h: 65 },
-          skinMap: { x: 20, y: 52, w: 4, h: 12 },
-          needsNewFormat: true,
-          mirrorFrom: 'rightLegFront',
+        right_leg_r: {
+          label: 'RR',
+          drawRegion: { x: 151, y: 355, w: 64, h: 128 },
+          skinMap: { x: 8, y: 20, w: 4, h: 12 },
         },
-        leftLegLeft: {
-          label: 'L',
-          drawRegion: { x: 477, y: 372, w: 65, h: 65 },
-          skinMap: { x: 24, y: 52, w: 4, h: 12 },
-          needsNewFormat: true,
-          mirrorFrom: 'rightLegRight',
+        right_leg_f: {
+          label: 'RF',
+          drawRegion: { x: 217, y: 355, w: 64, h: 128 },
+          skinMap: { x: 12, y: 20, w: 4, h: 12 },
         },
-        leftLegBack: {
-          label: 'B',
-          drawRegion: { x: 570, y: 372, w: 65, h: 65 },
-          skinMap: { x: 28, y: 52, w: 4, h: 12 },
-          needsNewFormat: true,
-          mirrorFrom: 'rightLegBack',
+        right_leg_u: {
+          label: 'RU',
+          drawRegion: { x: 217, y: 289, w: 64, h: 64 },
+          skinMap: { x: 4, y: 16, w: 4, h: 4 },
         },
-        leftLegRight: {
-          label: 'R',
-          drawRegion: { x: 663, y: 372, w: 65, h: 65 },
-          skinMap: { x: 16, y: 52, w: 4, h: 12 },
-          needsNewFormat: true,
-          mirrorFrom: 'rightLegLeft',
-        },
-        rightLowerLeg: {
-          label: 'D',
-          drawRegion: { x: 214, y: 484, w: 65, h: 65 },
+        right_leg_d: {
+          label: 'RD',
+          drawRegion: { x: 217, y: 485, w: 64, h: 64 },
           skinMap: { x: 8, y: 16, w: 4, h: 4 },
         },
-        leftLowerLeg: {
-          label: 'D',
-          drawRegion: { x: 307, y: 484, w: 65, h: 65 },
+
+        left_leg_l: {
+          label: 'LL',
+          drawRegion: { x: 308, y: 355, w: 64, h: 128 },
+          skinMap: { x: 16, y: 52, w: 4, h: 12 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_leg_l',
+        },
+        left_leg_b: {
+          label: 'LB',
+          drawRegion: { x: 374, y: 355, w: 64, h: 128 },
+          skinMap: { x: 20, y: 52, w: 4, h: 12 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_leg_b',
+        },
+        left_leg_r: {
+          label: 'LR',
+          drawRegion: { x: 440, y: 355, w: 64, h: 128 },
+          skinMap: { x: 24, y: 52, w: 4, h: 12 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_leg_r',
+        },
+        left_leg_f: {
+          label: 'LF',
+          drawRegion: { x: 506, y: 355, w: 64, h: 128 },
+          skinMap: { x: 28, y: 52, w: 4, h: 12 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_leg_f',
+        },
+        left_leg_u: {
+          label: 'LU',
+          drawRegion: { x: 308, y: 289, w: 64, h: 64 },
+          skinMap: { x: 20, y: 48, w: 4, h: 4 },
+          needsNewFormat: true,
+          mirrorFrom: 'right_leg_u',
+        },
+        left_leg_d: {
+          label: 'LD',
+          drawRegion: { x: 308, y: 485, w: 64, h: 64 },
           skinMap: { x: 24, y: 48, w: 4, h: 4 },
           needsNewFormat: true,
-          mirrorFrom: 'rightLowerLeg',
+          mirrorFrom: 'right_leg_d',
         },
       },
       extraLabels: [
